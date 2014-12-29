a87c4f267ef0af07bddab4b022e4278f3e0a6734
Morten Hustveit <morten.hustveit@gmail.com>
2014-12-29 17:45:49 +0100

    index.php: Reorganized code

--- index.php
+++ index.php
@@ -5,69 +5,67 @@ header('Content-Type: text/html; charset=UTF-8');
 // attacks on the unencrypted door.bitraf.no redirect domain.
 header('Strict-Transport-Security: max-age=31536000');
 
+// Set to a string when the user is identified.  Note that not all users are 
+// authorized to unlock the door.
+unset($account);
+
 // Set to true when authentication is complete.
 $ok = false;
 
-if (array_key_exists('action', $_POST) && $_POST['action'] == 'unlock')
+if (isset($_POST['action'])
+    && isset($_POST['pin'])
+    && $_POST['action'] == 'unlock')
 {
-  if (array_key_exists('pin', $_POST))
+  pg_connect('dbname=p2k12 user=p2k12');
+
+  $res = pg_query_params("SELECT COUNT(*) FROM auth_log WHERE host = $1 AND account IS NULL AND date > NOW() - INTERVAL '6 hour'", array($_SERVER['REMOTE_ADDR']));
+
+  $fail_count = pg_fetch_result($res, 0, 0);
+
+  if ($fail_count < 3)
   {
-    pg_connect('dbname=p2k12 user=p2k12');
+    $res = @pg_query("SELECT a.account, a.data FROM auth a LEFT JOIN (SELECT account, MAX(date) AS date, COUNT(account) AS logins FROM auth_log al WHERE al.date > current_date - interval '2 weeks' GROUP BY account) al ON al.account = a.account WHERE a.realm = 'door' ORDER BY al.logins DESC NULLS LAST");
 
-    $res = pg_query_params("SELECT COUNT(*) FROM auth_log WHERE host = $1 AND account IS NULL AND date > NOW() - INTERVAL '6 hour'", array($_SERVER['REMOTE_ADDR']));
-
-    $fail_count = pg_fetch_result($res, 0, 0);
-
-    if ($fail_count < 3)
+    while ($row = pg_fetch_assoc($res))
     {
-      $res = @pg_query("SELECT a.account, a.data FROM auth a LEFT JOIN (SELECT account, MAX(date) AS date, COUNT(account) AS logins FROM auth_log al WHERE al.date > current_date - interval '2 weeks' GROUP BY account) al ON al.account = a.account WHERE a.realm = 'door' ORDER BY al.logins DESC NULLS LAST");
-
-      unset ($account);
-
-      while ($row = pg_fetch_assoc($res))
+      if (crypt($_POST['pin'], $row['data']) === $row['data'])
       {
-        if (crypt($_POST['pin'], $row['data']) === $row['data'])
-        {
-          $account = $row['account'];
+        $account = $row['account'];
 
-          break;
-        }
-      }
-
-      if (isset($account))
-      {
-        $res = @pg_query_params("SELECT 1 FROM active_members WHERE account = $1 AND (price > 0 OR flag != '')", array($account));
-
-        if (pg_num_rows($res) == 1)
-          $ok = true;
-        else
-          $error = 'Error: Only registered and paying members can use door';
-      }
-
-      if ($ok)
-      {
-        @pg_query_params("INSERT INTO auth_log (host, account, realm) VALUES ($1, $2, 'door')", array($_SERVER['REMOTE_ADDR'], $account));
-      }
-      else
-      {
-        @pg_query_params("INSERT INTO auth_log (host, realm) VALUES ($1, 'door')", array($_SERVER['REMOTE_ADDR']));
-
-        $error = 'Incorrect password';
+        break;
       }
     }
+
+    if (isset($account))
+    {
+      $res = @pg_query_params("SELECT 1 FROM active_members WHERE account = $1 AND (price > 0 OR flag != '')", array($account));
+
+      if (pg_num_rows($res) == 1)
+        $ok = true;
+      else
+        $error = 'Error: Only registered and paying members can use door';
+    }
     else
-      $error = "Error: Too many login failures";
+    {
+      $error = 'Error: Incorrect password';
+    }
   }
-
-  if ($ok)
+  else
   {
-    $error = "Unlocked " . strftime('%H:%M:%S');
-
-    @pg_query_params('INSERT INTO checkins (account) VALUES ($1)', array($account));
-
-    $output = system("/usr/local/bin/bitraf-door-open.sh > /var/www/t 2>&1 &");
-    echo "<p>Welcome to Bitraf.</p>";
+    $error = "Error: Too many login failures";
   }
+}
+
+if ($ok)
+{
+  @pg_query_params("INSERT INTO auth_log (host, account, realm) VALUES ($1, $2, 'door')", array($_SERVER['REMOTE_ADDR'], $account));
+  @pg_query_params('INSERT INTO checkins (account) VALUES ($1)', array($account));
+
+  system("/usr/local/bin/bitraf-door-open.sh &");
+}
+else
+{
+  @pg_query_params("INSERT INTO auth_log (host, realm) VALUES ($1, 'door')", array($_SERVER['REMOTE_ADDR']));
 }
 ?>
 <!DOCTYPE HTML>
@@ -83,7 +81,7 @@ if (array_key_exists('action', $_POST) && $_POST['action'] == 'unlock')
   </head>
   <body>
   <? if ($ok): ?>
-    <p>Door is open.</p>
+    <p>Door is open.  Welcome to Bitraf (<?=strftime('%H:%M:%S')?>).</p>
   <? else: ?>
     <? if (isset($error)): ?>
       <p><?=$error?></p>
